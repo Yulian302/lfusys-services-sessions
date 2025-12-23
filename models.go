b09bf96e32f5450ced0c939117c539510c104cd9
Yulian302 <bohomolyulian3022003@gmail.com>
2025-12-23 11:34:17 +0100

    fix: remove uploaded_chunks attribute from empty initialization

--- models.go
+++ models.go
@@ -5,11 +5,11 @@ import "time"
 // UploadSession represents a multipart upload session
 // Status values:
 type UploadSession struct {
-	UploadId       string    `dynamodbav:"upload_id"`       // Unique identifier for upload session
-	UserEmail      string    `dynamodbav:"user_email"`      // Email(id) of user who owns this upload
-	FileSize       uint64    `dynamodbav:"file_size"`       // Total file size in bytes (max 10GB)
-	TotalChunks    uint32    `dynamodbav:"total_chunks"`    // Number of 5MB chunks required
-	UploadedChunks []string  `dynamodbav:"uploaded_chunks"` // List of successfully uploaded chunk ETags
+	UploadId    string `dynamodbav:"upload_id"`    // Unique identifier for upload session
+	UserEmail   string `dynamodbav:"user_email"`   // Email(id) of user who owns this upload
+	FileSize    uint64 `dynamodbav:"file_size"`    // Total file size in bytes (max 10GB)
+	TotalChunks uint32 `dynamodbav:"total_chunks"` // Number of 5MB chunks required
+	// UploadedChunks []int64   `dynamodbav:"uploaded_chunks,numberset"` // Bitmask of uploaded chunks (in bytes)
 	ExpirationTime time.Time `dynamodbav:"expiration_time"` // Session expires after 24 hours
 	CreatedAt      time.Time `dynamodbav:"created_at"`      // Session creation timestamp
 	Status         string    `dynamodbav:"status"`          // Current upload status
