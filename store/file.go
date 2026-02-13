c149ece58453bf0db4072ba9cb0c157552a8d37d
Yulian302 <bohomolyulian3022003@gmail.com>
2026-02-13 20:41:32 +0100

    feat: add retries to store handlers
    - Add retries with default delay and number of attempts to critical transient
      network requests
    - Add retries with smaller delay and number of attempts to health checks

--- store/file.go
+++ store/file.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"time"
 
-	"github.com/Yulian302/lfusys-services-commons/errors"
+	apperror "github.com/Yulian302/lfusys-services-commons/errors"
 	"github.com/Yulian302/lfusys-services-commons/health"
+	"github.com/Yulian302/lfusys-services-commons/retries"
 	"github.com/Yulian302/lfusys-services-sessions/models"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
@@ -37,11 +38,19 @@ func (s *DynamoDbFileStoreImpl) IsReady(ctx context.Context) error {
 	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
 	defer cancel()
 
-	_, err := s.client.DescribeTable(ctx, &dynamodb.DescribeTableInput{
-		TableName: aws.String(s.tableName),
-	})
+	return retries.Retry(
+		ctx,
+		retries.HealthAttempts,
+		retries.HealthBaseDelay,
+		func() error {
+			_, err := s.client.DescribeTable(ctx, &dynamodb.DescribeTableInput{
+				TableName: aws.String(s.tableName),
+			})
 
-	return err
+			return err
+		},
+		retries.IsRetriableDbError,
+	)
 }
 
 func (s *DynamoDbFileStoreImpl) Name() string {
@@ -49,22 +58,33 @@ func (s *DynamoDbFileStoreImpl) Name() string {
 }
 
 func (s *DynamoDbFileStoreImpl) Get(ctx context.Context, uploadId string) (*models.File, error) {
-	out, err := s.client.GetItem(ctx, &dynamodb.GetItemInput{
-		TableName: aws.String(s.tableName),
-		Key: map[string]types.AttributeValue{
-			"upload_id": &types.AttributeValueMemberS{Value: uploadId},
-		},
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	if out.Item == nil {
-		return nil, errors.ErrSessionNotFound
-	}
-
 	var file models.File
-	if err = attributevalue.UnmarshalMap(out.Item, &file); err != nil {
+
+	err := retries.Retry(
+		ctx,
+		retries.DefaultAttempts,
+		retries.DefaultBaseDelay,
+		func() error {
+			out, err := s.client.GetItem(ctx, &dynamodb.GetItemInput{
+				TableName: aws.String(s.tableName),
+				Key: map[string]types.AttributeValue{
+					"upload_id": &types.AttributeValueMemberS{Value: uploadId},
+				},
+			})
+			if err != nil {
+				return err
+			}
+
+			if out.Item == nil {
+				return apperror.ErrSessionNotFound
+			}
+
+			return attributevalue.UnmarshalMap(out.Item, &file)
+		},
+		retries.IsRetriableDbError,
+	)
+
+	if err != nil {
 		return nil, err
 	}
 
@@ -72,35 +92,53 @@ func (s *DynamoDbFileStoreImpl) Get(ctx context.Context, uploadId string) (*mode
 }
 
 func (s *DynamoDbFileStoreImpl) Create(ctx context.Context, file models.File) error {
-
 	fileItem, err := attributevalue.MarshalMap(file)
 	if err != nil {
 		return err
 	}
 
-	_, err = s.client.PutItem(ctx, &dynamodb.PutItemInput{
-		TableName: aws.String(s.tableName),
-		Item:      fileItem,
-	})
-	return err
+	return retries.Retry(
+		ctx,
+		retries.DefaultAttempts,
+		retries.DefaultBaseDelay,
+		func() error {
+			_, err = s.client.PutItem(ctx, &dynamodb.PutItemInput{
+				TableName: aws.String(s.tableName),
+				Item:      fileItem,
+			})
+			return err
+		},
+		retries.IsRetriableDbError,
+	)
 }
 
 func (s *DynamoDbFileStoreImpl) Read(ctx context.Context, ownerEmail string) ([]models.File, error) {
-	out, err := s.client.Query(ctx, &dynamodb.QueryInput{
-		TableName:              aws.String(s.tableName),
-		IndexName:              aws.String("owner_email-index"),
-		KeyConditionExpression: aws.String("owner_email = :e"),
-		ExpressionAttributeValues: map[string]types.AttributeValue{
-			":e": &types.AttributeValueMemberS{
-				Value: ownerEmail,
-			},
-		},
-	})
-	if err != nil {
-		return nil, err
-	}
 	var files []models.File
-	if err = attributevalue.UnmarshalListOfMaps(out.Items, &files); err != nil {
+
+	err := retries.Retry(
+		ctx,
+		retries.DefaultAttempts,
+		retries.DefaultBaseDelay,
+		func() error {
+			out, err := s.client.Query(ctx, &dynamodb.QueryInput{
+				TableName:              aws.String(s.tableName),
+				IndexName:              aws.String("owner_email-index"),
+				KeyConditionExpression: aws.String("owner_email = :e"),
+				ExpressionAttributeValues: map[string]types.AttributeValue{
+					":e": &types.AttributeValueMemberS{
+						Value: ownerEmail,
+					},
+				},
+			})
+			if err != nil {
+				return err
+			}
+			return attributevalue.UnmarshalListOfMaps(out.Items, &files)
+		},
+		retries.IsRetriableDbError,
+	)
+
+	if err != nil {
 		return nil, err
 	}
 
