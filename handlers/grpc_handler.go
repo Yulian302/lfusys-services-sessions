a3f796e5602803c2adf413d0a13bb41d16e07a80
Yulian302 <bohomolyulian3022003@gmail.com>
2026-02-17 15:33:18 +0100

    feat: Remove Upload url construction

--- handlers/grpc_handler.go
+++ handlers/grpc_handler.go
@@ -38,11 +38,7 @@ func (h *GrpcHandler) StartUpload(ctx context.Context, req *pb.UploadRequest) (*
 	totalChunks := (req.FileSize + chunkSize - 1) / chunkSize
 	uuidGen := uuid.New()
 	uploadId := uuidGen.String()
-	uploadUrls := make([]string, totalChunks)
-	for i := uint64(0); i < totalChunks; i++ {
-		// load balancer url in PROD, single worker url in DEV
-		uploadUrls[i] = fmt.Sprintf("%s/upload/%s/chunk/%d", h.uploadsUrl, uploadId, i+1)
-	}
+
 	var uploadSession models.UploadSession = models.UploadSession{
 		UploadId:    uploadId,
 		UserEmail:   req.UserEmail,
@@ -59,7 +55,7 @@ func (h *GrpcHandler) StartUpload(ctx context.Context, req *pb.UploadRequest) (*
 		return nil, err
 	}
 
-	return &pb.UploadReply{TotalChunks: uint32(totalChunks), UploadUrls: uploadUrls, UploadId: uploadId}, nil
+	return &pb.UploadReply{TotalChunks: uint32(totalChunks), UploadId: uploadId}, nil
 }
 
 func (h *GrpcHandler) GetUploadStatus(ctx context.Context, upload *pb.UploadID) (*pb.StatusReply, error) {
